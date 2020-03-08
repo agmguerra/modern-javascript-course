10b24d2b6ee060afc0edc6854a678c67e994e98a
AlexandreGuerra <agmguerra@gmail.com>
2020-03-07 23:12:24 -0300

    novo modulo do curso e aplic de exemplo anteior

---
+++ userform/app.js
@@ -0,0 +1,50 @@
+//Form blur eventlistners
+document.getElementById('name').addEventListener('blur',validateName);
+document.getElementById('zipcode').addEventListener('blur',validateZip);
+document.getElementById('email').addEventListener('blur',validateEmail);
+document.getElementById('phonenumber').addEventListener('blur',validatePhone);
+
+//event handling functions
+function validateName() {
+  const name = document.getElementById('name');
+  const re = /^[a-zA-Z]{2,10}$/;
+
+  if (!re.test(name.value)) {
+    name.classList.add('is-invalid');
+  } else {
+    name.classList.remove('is-invalid');
+  }
+}
+
+function validateZip() {
+  const zipcode = document.getElementById('zipcode');
+  const re = /^[0-9]{5}(-[0-9]{3}|[0-9]{3})$/;
+
+  if (!re.test(zipcode.value)) {
+    zipcode.classList.add('is-invalid');
+  } else {
+    zipcode.classList.remove('is-invalid');
+  }
+}
+
+function validateEmail() {
+  const email = document.getElementById('email');
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  if (!re.test(email.value)) {
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
+}
+
+function validatePhone() {
+  const phone = document.getElementById('phonenumber');
+  const re = /^\(?\d{2}\)?[-. ]?\d{3,5}[-. ]?\d{4}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
+}
